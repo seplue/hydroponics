8a8c79b8bdbd37b2e2ed496011d364a19cf15e40
Severin Plüss <severin.pluess@bluewin.ch>
2022-06-01 13:42:10 +0200

    new main() routine

--- main_development.py
+++ main_development.py
@@ -7,21 +7,38 @@ import sensors
 import measurement_handling
 
 development = True
-
-wifi.connect()
-
-pump.pump(dev=development)
-measurements = sensors.measure_all()
-measurement_handling.write_json(measurements)
-print(sensors.measure_all())
-
-for x in range(12):
-    measurement_handling.send_json("a")
-    time.sleep(5*60)
-
-
-
 """
+# only pump
+def main():
+    
+    while True:
+        pump.pump(60, 60, False)
+        
+# do everything
+"""
+def main():
+    sent_since_measurement = False
+    wifi.connect()
+
+    while True:
+        pump.pump(60, 0, dev=development)
+        
+        measurements = sensors.measure_all()
+        print(measurements)
+        measurement_handling.write_json(measurements)
+        
+        for x in range(12):
+            #check if not sent_since_measurement
+            #check if internet connected, if not try reconnecting
+            # if connected, send measurements
+            # if no more measurements in MEASUREMENT.py set sent_since_measurement = True
+            # wait for 5 min
+            measurement_handling.send_json("a")
+            time.sleep(5*60)
+"""
+
+
+
 connect to wifi
     get internet time
 measure
